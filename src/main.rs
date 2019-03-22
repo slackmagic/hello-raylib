6b22dd40af255dc6c4ce6d6044d596b3ffcb900c
slackmagic <slack.magic@gmail.com>
2019-03-22 19:12:07 +0100

    Update hello world

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
 use raylib::Color;
 
 fn main() {
-    println!("Hello, world!");
+    with_raylib_handler(|rl| {
+        while !rl.window_should_close() {
+            update_context();
+            draw_screen(&rl);
+        }
+    });
+}
+
+fn update_context() {
+    //DO SOME STUFF
+}
+
+fn draw_screen(rl: &raylib::RaylibHandle) {
+    draw_in_2d(&rl, |rl| {
+        rl.clear_background(Color::BEIGE);
+        rl.draw_rectangle(0, 0, 300, 200, Color::ORANGE);
+        rl.draw_text("Hello world from RUST !", 12, 12, 20, Color::BROWN);
+        rl.draw_text(&rl.get_time().to_string(), 12, 32, 8, Color::RED);
+        rl.draw_fps(500, 0);
+    });
+}
+
+fn with_raylib_handler<F, T>(func: F) -> T
+where
+    F: FnOnce(&raylib::RaylibHandle) -> T,
+{
+    let rl = init();
+    let ret: T = func(&rl);
+    ret
+}
+
+fn init() -> raylib::RaylibHandle {
     let rl = raylib::init()
-        .size(640, 480)
+        //.size(640, 480)
+        .size(1920, 1024)
         .title("Hello, World from RUST")
         .build();
 
-    while !rl.window_should_close() {
-        rl.begin_drawing();
+    rl.hide_cursor();
+    rl.toggle_fullscreen();
+    rl
+}
 
-        rl.clear_background(Color::BEIGE);
-        rl.draw_text("Hello world from RUST !", 12, 12, 20, Color::BROWN);
-
-        rl.end_drawing();
-    }
+fn draw_in_2d<F, T>(rl: &raylib::RaylibHandle, func: F) -> T
+where
+    F: Fn(&raylib::RaylibHandle) -> T,
+{
+    rl.begin_drawing();
+    //Begin DRAWING;
+    let ret: T = func(&rl);
+    //End DRAWING;
+    rl.end_drawing();
+    ret
 }
