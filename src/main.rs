5614ac71f782688182079f512fa15a09c7a9b17e
slackmagic <slack.magic@gmail.com>
2019-03-22 19:26:06 +0100

    Update bis

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,13 @@ fn update_context() {
 }
 
 fn draw_screen(rl: &raylib::RaylibHandle) {
-    draw_in_2d(&rl, |rl| {
+    draw_in_2d(&rl, |rl: &raylib::RaylibHandle| {
         rl.clear_background(Color::BEIGE);
+
         rl.draw_rectangle(0, 0, 300, 200, Color::ORANGE);
         rl.draw_text("Hello world from RUST !", 12, 12, 20, Color::BROWN);
         rl.draw_text(&rl.get_time().to_string(), 12, 32, 8, Color::RED);
+        rl.set_window_title(&rl.get_time().to_string());
         rl.draw_fps(500, 0);
     });
 }
@@ -34,13 +36,13 @@ where
 
 fn init() -> raylib::RaylibHandle {
     let rl = raylib::init()
-        //.size(640, 480)
-        .size(1920, 1024)
+        .size(640, 480)
+        //.size(1920, 1024)
         .title("Hello, World from RUST")
         .build();
 
     rl.hide_cursor();
-    rl.toggle_fullscreen();
+    //rl.toggle_fullscreen();
     rl
 }
 
