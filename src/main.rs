ed567a74a3a065650d6c75875cb703c08e05cbc8
slackmagic <slack.magic@gmail.com>
2019-03-26 07:58:14 +0100

    Update for font

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,24 @@ fn update_context() {
 fn draw_screen(rl: &raylib::RaylibHandle) {
     draw_in_2d(&rl, |rl: &raylib::RaylibHandle| {
         rl.clear_background(Color::BEIGE);
-
         rl.draw_rectangle(0, 0, 300, 200, Color::ORANGE);
         rl.draw_text("Hello world from RUST !", 12, 12, 20, Color::BROWN);
         rl.draw_text(&rl.get_time().to_string(), 12, 32, 8, Color::RED);
         rl.set_window_title(&rl.get_time().to_string());
         rl.draw_fps(500, 0);
+
+        let font = rl.load_font_ex("ThisSmacky.ttf", 16, Some(&[0, 0]));
+        //let font = rl.load_font_ex("I-pixel-u.ttf", 65, Some(&[0, 0]));
+        let position = raylib::Vector2 { x: 20.0, y: 50.0 };
+
+        rl.draw_text_ex(
+            &font,
+            "HELLO",
+            position,
+            (font.baseSize * 4) as f32,
+            0.0,
+            Color::BLACK,
+        );
     });
 }
 
