d2324dda4102163d866faa04f27e5ad203eafba7
SlackMagiC <laurent.pietrzyk@gmail.com>
2019-03-26 20:58:10 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 use raylib::Color;
 
 fn main() {
+    bench_launch();
+}
+
+fn bench_launch() {
+    let rl = raylib::init()
+        .size(640, 480)
+        //.size(1920, 1024)
+        .title("Hello, World from RUST")
+        .build();
+
+    rl.set_target_fps(60);
+
+    let message = "Hello From RUST and Raylib:";
+    let message2 = "What an amazing and promising synergy featuring SlackMagiC !";
+    let font = rl.load_font_ex("I-pixel-u.ttf", 80, None);
+    let font2 = rl.load_font_ex("ThisSmacky.ttf", 65, None);
+
+    let position = raylib::Vector2 { x: 20.0, y: 70.0 };
+    let position2 = raylib::Vector2 { x: 20.0, y: 50.0 };
+
+    while !rl.window_should_close() {
+        rl.begin_drawing();
+
+        rl.clear_background(Color::BLACK);
+        rl.draw_text_ex(&font, &message, position2, 20.0, 0.0, Color::BEIGE);
+        rl.draw_text_ex(&font2, &message2, position, 13.0, 0.0, Color::BROWN);
+
+        rl.draw_fps(500, 0);
+        rl.end_drawing();
+    }
+}
+
+fn proper_launch() {
     with_raylib_handler(|rl| {
         while !rl.window_should_close() {
             update_context();
@@ -20,14 +53,13 @@ fn draw_screen(rl: &raylib::RaylibHandle) {
         rl.draw_text("Hello world from RUST !", 12, 12, 20, Color::BROWN);
         rl.draw_text(&rl.get_time().to_string(), 12, 32, 8, Color::RED);
         rl.set_window_title(&rl.get_time().to_string());
-        rl.draw_fps(500, 0);
 
         let font = rl.load_font_ex("ThisSmacky.ttf", 16, Some(&[0, 0]));
-        //let font = rl.load_font_ex("I-pixel-u.ttf", 65, Some(&[0, 0]));
+        let font2 = rl.load_font("I-pixel-u.ttf");
         let position = raylib::Vector2 { x: 20.0, y: 50.0 };
 
         rl.draw_text_ex(
-            &font,
+            &font2,
             "HELLO",
             position,
             (font.baseSize * 4) as f32,
@@ -54,6 +86,7 @@ fn init() -> raylib::RaylibHandle {
         .build();
 
     rl.hide_cursor();
+    rl.set_target_fps(60);
     //rl.toggle_fullscreen();
     rl
 }
